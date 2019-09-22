9383395af457622078fbad4283a67d3e3550e271
misaka2ang <1105654071@qq.com>
2019-09-22 11:47:05 +0800

    hw1

---
+++ eureka-demo/eureka-server/src/main/java/com/mingyin/EurekaServer.java
@@ -0,0 +1,20 @@
+package com.mingyin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaServer
+@RestController
+public class EurekaServer {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+
+    }
+}
